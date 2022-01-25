1f2a9aeac1871a6e0c8ca31db470a94beca899f5
sudarat54 <636051200229@mail.rmutk.ac.th>
2022-01-25 12:55:11 +0700

    Initial commit

---
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,102 @@
+package com.example.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button32.setOnClickListener {
+            textView4.setText("0")
+        }
+        button36.setOnClickListener {
+            if (textView4.text == "0") {
+                textView4.setText("")
+                textView4.setText(textView4.text.toString() + "7")
+            } else {
+                textView4.setText(textView4.text.toString() + "7")
+            }
+        }
+        button37.setOnClickListener {
+            if (textView4.text == "0") {
+                textView4.setText("")
+                textView4.setText(textView4.text.toString() + "8")
+            } else {
+                textView4.setText(textView4.text.toString() + "8")
+            }
+        }
+        button38.setOnClickListener {
+            if (textView4.text == "0") {
+                textView4.setText("")
+                textView4.setText(textView4.text.toString() + "9")
+            } else {
+                textView4.setText(textView4.text.toString() + "9")
+            }
+        }
+        button40.setOnClickListener {
+            if (textView4.text == "0") {
+                textView4.setText("")
+                textView4.setText(textView4.text.toString() + "4")
+            } else {
+                textView4.setText(textView4.text.toString() + "4")
+            }
+        }
+        button41.setOnClickListener {
+            if (textView4.text == "0") {
+                textView4.setText("")
+                textView4.setText(textView4.text.toString() + "5")
+            } else {
+                textView4.setText(textView4.text.toString() + "5")
+            }
+        }
+        button42.setOnClickListener {
+            if (textView4.text == "0") {
+                textView4.setText("")
+                textView4.setText(textView4.text.toString() + "6")
+            } else {
+                textView4.setText(textView4.text.toString() + "6")
+            }
+        }
+        button44.setOnClickListener {
+            if (textView4.text == "0") {
+                textView4.setText("")
+                textView4.setText(textView4.text.toString() + "1")
+            } else {
+                textView4.setText(textView4.text.toString() + "1")
+            }
+        }
+        button45.setOnClickListener {
+            if (textView4.text == "0") {
+                textView4.setText("")
+                textView4.setText(textView4.text.toString() + "2")
+            } else {
+                textView4.setText(textView4.text.toString() + "2")
+            }
+        }
+        button46.setOnClickListener {
+            if (textView4.text == "0") {
+                textView4.setText("")
+                textView4.setText(textView4.text.toString() + "3")
+            } else {
+                textView4.setText(textView4.text.toString() + "3")
+            }
+        }
+        button51.setOnClickListener {
+            if (textView4.text == "0") {
+                textView4.setText("0")
+            } else {
+                textView4.setText(textView4.text.toString() + "0")
+            }
+        }
+        button33.setOnClickListener {
+            if (textView4.text == "0" || textView4.text.length.toString() == "1") {
+                textView4.setText("0")
+            } else {
+                textView4.setText(textView4.text.toString().dropLast(1))
+            }
+        }
+
+    }
+}
